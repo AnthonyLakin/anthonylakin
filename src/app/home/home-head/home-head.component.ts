f45af7032d37647482abac8d48bd5aa387bc19e2
anthonylakin <lakindevelopments@gmail.com>
2023-07-26 01:28:10 -0500

    Updating angular core

--- src/app/home/home-head/home-head.component.ts
+++ src/app/home/home-head/home-head.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MatGridListModule} from "@angular/material/grid-list";
+
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+  border: string;
+}
 
 @Component({
   selector: 'app-home-head',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage, MatGridListModule],
   templateUrl: './home-head.component.html',
   styleUrls: ['./home-head.component.css']
 })
 export class HomeHeadComponent {
-
+  tiles: Tile[] = [
+    {text: 'Tile 1', cols: 2, rows: 1 ,border: '3px double purple'},
+    {text: 'Tile 2', cols: 2, rows: 1 ,border: '3px double red'},
+    {text: 'Tile 3', cols: 2, rows: 1 ,border: '3px double skyblue'},
+    {text: 'Tile 4', cols: 2, rows: 1 ,border: '3px double yellow'},
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import {Component } from '@angular/core';
+import {HomeHeadComponent} from "./home-head/home-head.component";
 
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    standalone: true,
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [
+    HomeHeadComponent
+  ]
 })
 export class HomeComponent {
 
